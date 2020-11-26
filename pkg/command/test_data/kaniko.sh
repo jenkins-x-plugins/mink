c1296374561f186b5d3557dae83eefb07103115b
James Strachan <james.strachan@gmail.com>
2020-11-26 18:22:34 +0000

    fix: lets use bash

--- pkg/command/test_data/kaniko.sh
+++ pkg/command/test_data/kaniko.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # a sample digest for testing
 digest="sha256:8e65ec4b80519d869e8d600fdf262c6e8cd3f6c7e8382406d9cb039f352a69bc"
@@ -7,7 +7,7 @@ echo "fake-kaniko building digest $digest"
 
 for arg in "$@"
 do
-  if  [[ $arg == "--digest-file="* ]] ;
+  if [[ $arg == "--digest-file="* ]] ;
   then
       df=${arg#"--digest-file="}
       echo "writing to digest file: $df"
